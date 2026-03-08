b2a5a22f3b218672504182bad078f5bb3057d4b3
Khan <Khan@KHAN>
2026-03-08 15:56:30 +0500

    Navigation Changes

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from '../screens/auth/SplashScreen';
 import LoginScreen from '../screens/auth/LoginScreen';
 import SignUpScreen from '../screens/auth/SignUpScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const AuthStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Splash" component={SplashScreen} />
+      {/* <Stack.Screen name="Splash" component={SplashScreen} /> */}
       <Stack.Screen name="SignUp" component={SignUpScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,31 +1,70 @@
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import {createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { useSelector } from 'react-redux';
+
+// import AuthStack from './AuthStack';
+// import RiderHomeScreen from '../screens/main/Rider/RiderHomeScreen';
+// import DriverHomeScreen from '../screens/main/Driver/DriverHomeScreen'
+// import SplashScreen from '../screens/auth/SplashScreen';
+
+// const Stack = createNativeStackNavigator();
+
+// const RootNavigator = () => {
+//   const { isLoggedIn, userData } = useSelector(state => state.user);
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        
+//         <Stack.Screen name="Splash" component={SplashScreen} />
+
+        
+//         <Stack.Screen name="Auth" component={AuthStack} />
+
+        
+    
+//         <Stack.Screen name="RiderHome" component={RiderHomeScreen} />
+//         <Stack.Screen name="DriverHome" component={DriverHomeScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default RootNavigator;
+
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useSelector } from 'react-redux';
 
 import AuthStack from './AuthStack';
 import RiderHomeScreen from '../screens/main/Rider/RiderHomeScreen';
-import DriverHomeScreen from '../screens/main/Driver/DriverHomeScreen'
+import DriverHomeScreen from '../screens/main/Driver/DriverHomeScreen';
 import SplashScreen from '../screens/auth/SplashScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
-  const { isLoggedIn, userData } = useSelector(state => state.user);
+  const { isLoggedIn, userData, isFirstTime } = useSelector(state => state.user);
 
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        
-        <Stack.Screen name="Splash" component={SplashScreen} />
-
-        
-        <Stack.Screen name="Auth" component={AuthStack} />
-
-        
-    
-        <Stack.Screen name="RiderHome" component={RiderHomeScreen} />
-        <Stack.Screen name="DriverHome" component={DriverHomeScreen} />
+        {isFirstTime ? (
+          <Stack.Screen name="Splash" component={SplashScreen} />
+        ) : !isLoggedIn ? (
+          <Stack.Screen name="Auth" component={AuthStack} />
+        ) : (
+          <>
+            {userData?.role === 'driver' ? (
+              <Stack.Screen name="DriverHome" component={DriverHomeScreen} />
+            ) : (
+              <Stack.Screen name="RiderHome" component={RiderHomeScreen} />
+            )}
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
